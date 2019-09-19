d83765f222a97881144c8119d2612ec0a352bb84
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2019-09-19 16:30:21 -0500

    Estructura del proyecto

---
+++ src/main/java/edu/eci/arsw/synchdrive/SynchdriveApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.synchdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynchdriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/eci/arsw/synchdrive/SynchdriveApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.eci.arsw.synchdrive;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SynchdriveApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
