97d5b169d0804dd0a6b3c5d9837077db4a98a61b
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2019-09-30 14:58:05 -0500

    JWT auth implementation

---
+++ src/main/java/edu/eci/arsw/synchdrive/security/config/JwtUserDetailsService.java
@@ -0,0 +1,37 @@
+package edu.eci.arsw.synchdrive.security.config;
+
+import edu.eci.arsw.synchdrive.persistence.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Component
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userDao;
+
+    @Autowired
+    private PasswordEncoder bcryptEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("javainuse".equals(username)) {
+            return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6",
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with username: " + username);
+        }
+    }
+
+
+
+}
