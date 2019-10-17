25afd8b37d0258d81abb7cc8be7e1b3230292e90
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2019-10-17 17:26:59 -0500

    Test for GET,POST for API (Customer,Car,Driver)

--- src/test/java/edu/eci/arsw/synchdrive/SynchdriveApplicationTests.java
+++ src/test/java/edu/eci/arsw/synchdrive/SynchdriveApplicationTests.java
@@ -2,6 +2,8 @@ package edu.eci.arsw.synchdrive;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.eci.arsw.synchdrive.model.Car;
+import edu.eci.arsw.synchdrive.model.Customer;
+import edu.eci.arsw.synchdrive.model.Driver;
 import edu.eci.arsw.synchdrive.security.config.JwtTokenUtil;
 import org.junit.Before;
 import org.junit.Test;
@@ -62,9 +64,137 @@ public class SynchdriveApplicationTests {
                 .andExpect(status().isCreated());
     }
 
+    @Test
+    public void searchCarByPlateTest() throws Exception{
+        Car testCar = new Car();
+        testCar.setPlate("123");
+        testCar.setModel("sport");
+        testCar.setSeats(2);
 
+        mvc.perform(MockMvcRequestBuilders
+                .post("/cars")
+                .header("Authorization",authToken)
+                .content(asJsonString(testCar))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
 
-    public static String asJsonString(final Object obj) {
+        //not found
+	    mvc.perform(MockMvcRequestBuilders
+                .get("/cars/not-a-plate")
+                .header("Authorization",authToken))
+                .andExpect(status().isNotFound());
+	    //found
+	    mvc.perform(MockMvcRequestBuilders
+                .get("/cars/123")
+                .header("Authorization",authToken))
+                .andExpect(status().isAccepted());
+    }
+
+    @Test
+    public void getAllCustomersTest() throws Exception{
+	    mvc.perform(MockMvcRequestBuilders
+                .get("/users")
+                .header("Authorization",authToken))
+                .andExpect(status().isAccepted());
+    }
+
+    @Test
+    public void createCustomerTest() throws Exception{
+        Customer testCustomer = new Customer();
+        testCustomer.setEmail("test@email.com");
+        testCustomer.setPassword("test-password");
+        testCustomer.setName("test");
+
+        mvc.perform(MockMvcRequestBuilders
+                .post("/users")
+                .header("Authorization",authToken)
+                .content(asJsonString(testCustomer))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void searchCustomerByEmail() throws Exception{
+        Customer testCustomer = new Customer();
+        testCustomer.setEmail("test2@email.com");
+        testCustomer.setPassword("test-password");
+        testCustomer.setName("test");
+
+        mvc.perform(MockMvcRequestBuilders
+                .post("/users")
+                .header("Authorization",authToken)
+                .content(asJsonString(testCustomer))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+	    //not found
+	    mvc.perform(MockMvcRequestBuilders
+                .get("/users/not-a-user")
+                .header("Authorization",authToken))
+                .andExpect(status().isNotFound());
+
+	    //found
+        mvc.perform(MockMvcRequestBuilders
+                .get("/users/test2@email.com")
+                .header("Authorization",authToken))
+                .andExpect(status().isAccepted());
+    }
+
+    @Test
+    public void getAllDriversTest() throws Exception{
+	    mvc.perform(MockMvcRequestBuilders
+                .get("/drivers")
+                .header("Authorization",authToken))
+                .andExpect(status().isAccepted());
+    }
+
+    @Test
+    public void createDriverTest() throws Exception{
+	    Driver testDriver = new Driver();
+	    testDriver.setEmail("driver@test.com");
+	    testDriver.setPassword("test");
+
+	    mvc.perform(MockMvcRequestBuilders
+                .post("/drivers")
+                .header("Authorization",authToken)
+                .content(asJsonString(testDriver))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void searchDriverByEmail() throws Exception{
+        Driver testDriver = new Driver();
+        testDriver.setEmail("driver1@test.com");
+        testDriver.setPassword("test");
+
+        //creating
+        mvc.perform(MockMvcRequestBuilders
+                .post("/drivers")
+                .header("Authorization",authToken)
+                .content(asJsonString(testDriver))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated());
+
+        //not found
+        mvc.perform(MockMvcRequestBuilders
+                .get("/drivers/not-a-driver")
+                .header("Authorization",authToken))
+                .andExpect(status().isNotFound());
+
+        //found
+        mvc.perform(MockMvcRequestBuilders
+                .get("/drivers/driver1@test.com")
+                .header("Authorization",authToken))
+                .andExpect(status().isAccepted());
+    }
+
+    //Method for making a json string
+    private static String asJsonString(final Object obj) {
         try {
             return new ObjectMapper().writeValueAsString(obj);
         } catch (Exception e) {
