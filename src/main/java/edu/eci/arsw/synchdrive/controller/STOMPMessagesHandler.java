b3cc3140bf969194c66a0d5266ee8e802ebb5c84
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2019-10-30 09:44:43 -0500

    Server side push test

--- src/main/java/edu/eci/arsw/synchdrive/controller/STOMPMessagesHandler.java
+++ src/main/java/edu/eci/arsw/synchdrive/controller/STOMPMessagesHandler.java
@@ -6,6 +6,9 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 @Controller
 public class STOMPMessagesHandler {
 
@@ -15,6 +18,18 @@ public class STOMPMessagesHandler {
     @MessageMapping("/services")
     public void handleServiceEvent(Servicio servicio){
         System.out.println("New service! " + servicio);
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Servicio servicio1 = new Servicio();
+                servicio1.setPrice(24.0);
+                servicio1.setDistance(78.09);
+                servicio1.setDuration(18.14);
+                msgt.convertAndSend("/topic/services",servicio1);
+            }
+        };
+        Timer timer = new Timer(true);
+        timer.schedule(timerTask,5000,3*100);
         //msgt.convertAndSend("/topic/services."+numService);
     }
 
