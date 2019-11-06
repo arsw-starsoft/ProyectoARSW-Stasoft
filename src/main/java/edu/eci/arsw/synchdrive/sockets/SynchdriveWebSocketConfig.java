3435455dc7e7565a8b9618464efcf49f1b8f3faa
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2019-11-06 00:12:17 -0500

    CORS Fix stomp

--- src/main/java/edu/eci/arsw/synchdrive/sockets/SynchdriveWebSocketConfig.java
+++ src/main/java/edu/eci/arsw/synchdrive/sockets/SynchdriveWebSocketConfig.java
@@ -2,11 +2,13 @@ package edu.eci.arsw.synchdrive.sockets;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
+@CrossOrigin
 @EnableWebSocketMessageBroker
 public class SynchdriveWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
@@ -20,8 +22,7 @@ public class SynchdriveWebSocketConfig implements WebSocketMessageBrokerConfigur
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/stompendpoint")
                 .setAllowedOrigins("*")
-                .withSockJS()
-                .setSupressCors(true);
+                .withSockJS();
     }
 
 }
