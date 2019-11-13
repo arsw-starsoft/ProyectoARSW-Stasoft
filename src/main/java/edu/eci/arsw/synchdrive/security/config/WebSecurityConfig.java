26e8deb91aae3aaaf81a6e9c2bc81c683a74db87
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2019-11-13 00:58:21 -0500

    security

--- src/main/java/edu/eci/arsw/synchdrive/security/config/WebSecurityConfig.java
+++ src/main/java/edu/eci/arsw/synchdrive/security/config/WebSecurityConfig.java
@@ -99,7 +99,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests().antMatchers("/app/**").permitAll().
 
 // all other requests need to be authenticated
-        //anyRequest().authenticated().
+        anyRequest().authenticated().
                 and().
 
 // make sure we use stateless session; session won't be used to
