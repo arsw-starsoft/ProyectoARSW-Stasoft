a957f3278d2691cde3824202fa5cb577d2d162ad
Julian Benitez <julian.benitez@mail.escuelaing.edu.co>
2019-11-26 22:36:22 -0500

    powershell script

---
+++ postman/uber/newman.ps1
@@ -0,0 +1,5 @@
+$peticiones = 10;
+$hilos = 10;
+For ($i = 0; $i -lt $hilos; $i++){
+    Start-Process 'cmd' -ArgumentList "/c start newman run .\uber.postman_collection.json -e .\[uber].postman_enviroment.json -n $peticiones";
+}
